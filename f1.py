22efa6f7e1257a039d864242484fe8fae41a7d4b
Jhansi-t21 <jhansireddy025@gmail.com>
2026-08-03 16:58:00 +0530

    feat:add numpy array operations

---
+++ f1.py
@@ -0,0 +1,21 @@
+#1D 2D 3D arrays
+
+import numpy as np
+
+arr1 = np.array([1,2,3,4,5])
+
+arr2 = np.array([[1,2,3],
+                 [4,5,6]])
+
+arr3 = np.array([
+    [[1,2],[3,4]],
+    [[5,6],[7,8]]
+])
+
+print(arr1)
+print(arr2)
+print(arr3)
+
+print(arr1.shape)
+print(arr2.shape)
+print(arr3.shape)
